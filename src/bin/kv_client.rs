76cb01de7427b981dafc6291f05458ad5dc31955
w1999wtw3537@sina.com <w1999wtw3537@sina.com>
2022-10-18 18:02:18 +0800

    kv_client 增加命令参数解析逻辑

--- src/bin/kv_client.rs
+++ src/bin/kv_client.rs
@@ -1,8 +1,9 @@
 use std::error::Error;
 use anyhow::Result;
 use bytes::BytesMut;
+use clap::Parser;
 use futures::{SinkExt, StreamExt};
-use kvserver::{ClientConfig, CmdRequest, CmdResponse};
+use kvserver::{ClientConfig, CmdRequest, CmdResponse, args::ClientArgs};
 use prost::Message;
 use tokio::net::TcpStream;
 use tokio_util::codec::{Framed, LengthDelimitedCodec};
@@ -27,13 +28,16 @@ async fn main() ->Result<(),Box<dyn Error>> {
   // let length =  tcpStream.read(&mut buffer).await.expect("读取信息失败!");
 
   // println!("客户端读取到的内容[{}],内容长度:[{}]",String::from_utf8(buffer).unwrap(),length);
-
+    // 解析命令行输入的内容
+    let client_args = ClientArgs::parse();
 
     // 使用Frame的LengthDelimitedCodec进行编解码操
     let mut stream = Framed::new(tcp_stream,LengthDelimitedCodec::new());
     let mut buf = BytesMut::new();
     // 创建 get 命令
-    let cmd_get= CmdRequest::get("key");
+    // let cmd_get= CmdRequest::get("key");
+    // 解析命令行输入的内容为客户端命令
+    let cmd_get = process_args(client_args).await?;
     cmd_get.encode(&mut buf).unwrap();
     info!("send message success!");
     // 发送get命令消息
@@ -45,4 +49,26 @@ async fn main() ->Result<(),Box<dyn Error>> {
     }
    
   Ok(())
+}
+
+async fn process_args(args: ClientArgs) -> Result<CmdRequest,Box<dyn Error>> {
+
+   match args {
+    ClientArgs::Get { key } => {
+      Ok(CmdRequest::get(key))
+    },
+    ClientArgs::Set { key, value } => {
+      Ok(CmdRequest::set(key, value.into(),1000))
+    },
+    ClientArgs::Publish { topic, value } => {
+      Ok(CmdRequest::publish(topic, value.into()))
+    },
+    ClientArgs::Subscribe { topic } => {
+      Ok(CmdRequest::subscribe(topic))
+    },
+    ClientArgs::UnSubscribe { topic, id } => {
+      Ok(CmdRequest::unsubscribe(topic, id.into()))
+    }
+   } 
+   
 }
