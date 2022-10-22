293411c470d17a4b5b11974b0158c8ffdb1f22cd
root <root@localhost.localdomain>
2022-10-22 17:17:41 +0800

    service封装实现

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,10 +1,55 @@
 mod cmd_service;
 
+use std::sync::Arc;
+
+use crate::CmdRequest;
 use crate::CmdResponse;
 use crate::Storage;
+use crate::cmd_request::ReqData;
+use crate::storage::rocks_db_storage::RocksDbStorage;
 
 // 定义命令执行公共行为
 pub trait CmdService {
     // 命令执行方法  &impl Storage 与&dyn Storage的区别 ？ TODO
     fn excute(self,stor: &dyn Storage) -> CmdResponse;
+}
+// 默认存储为RocksDbStorage
+pub struct Service<S = RocksDbStorage> {
+    store_srv: Arc<StorageService<S>>,
+}
+
+// 需要在多线程中clone
+pub struct StorageService<Store> {
+    store: Store,
+} 
+
+impl <Store: Storage> Service<Store> {
+    
+    pub fn new(store: Store) -> Self {
+        Self { store_srv: Arc::new(StorageService{store}) }
+    }
+
+    pub async fn excute(&self,cmd_req: CmdRequest) -> CmdResponse {
+        let cmd_res = process_cmd(&self.store_srv.store,cmd_req).await;
+        cmd_res
+    }
+   
+}
+
+impl<Store> Clone for Service<Store> {
+    fn clone(&self) -> Self {
+        Self { store_srv: self.store_srv.clone() }
+    }
+}
+
+async fn process_cmd(store: &impl Storage,cmd_req: CmdRequest) -> CmdResponse {
+    match cmd_req.req_data {
+        Some(ReqData::Get(cmd_get)) => {
+            cmd_get.excute(store)
+        },
+        Some(ReqData::Set(cmd_set)) => {
+            cmd_set.excute(store)
+        }
+    _ => "Invalid Command".into(),    
+    }
 }
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use bytes::Bytes;
 
 pub mod mem_storage;
-mod rocks_db_storage;
+pub(crate) mod rocks_db_storage;
 
 pub trait Storage {
     fn get(&self,key: &str) -> Result<Option<Bytes>, Box<dyn Error>>;
